0c252f9ae64fc2d5e0a1fcf5514412bdcb6dd05c
kostik-loyko <kostik.loyko@mail.ru>
2022-11-25 21:49:51 +0300

    feat: finish number test

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -86,8 +86,11 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  if (b === 0) {
+    return Math.abs((b / a) * -1);
+  }
+  return (b / a) * -1;
 }
 
 
@@ -109,8 +112,9 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.acos(((x1 * x2) + (y1 * y2))
+    / (Math.sqrt((x1 ** 2 + y1 ** 2)) * Math.sqrt((x2 ** 2 + y2 ** 2))));
 }
 
 /**
@@ -162,8 +166,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -184,8 +188,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / (10 ** pow)) * 10 ** pow;
 }
 
 /**
@@ -205,8 +209,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let flag = true;
+  for (let i = 1; i < n; i += 1) {
+    if (n % i === 0 && i !== 1 && n !== i) {
+      flag = false;
+    }
+  }
+  return flag;
 }
 
 /**
@@ -224,8 +234,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (typeof Number(value) === 'number' && !Object.is(Number(value), NaN)) {
+    return Number(value);
+  }
+  return def;
 }
 
 module.exports = {
